d924cda0bb4885cb7dd1e1c7dc491f5cb20673da
craniacshencil <rajnikamrajnikam@gmail.com>
2024-06-06 21:30:46 +0530

    Add lsp, formatting and linting
    - Lsp added through typecraft's episode 3
    - Formatting (for lua and python) and linting(for no-language) setup through typecraft's episode 4

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,5 @@ return{
     priority = 1000,
     config = function()
         vim.cmd.colorscheme "catppuccin"
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     end
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
 return{
     "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
     config = function()
-        local config = require("nvim-treesitter.configs") 
+        local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "c", "lua", "css", "go", "python", "javascript", "html", "vue" },
+            auto_install = true,
             highlight = { enable = true },
-            indent = { enable = true },  
+            indent = { enable = true },
         })
     end
 }
