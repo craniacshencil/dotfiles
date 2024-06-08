a68112ad2bb2ac3479200d4a742ecf35fedc0768
craniacshencil <rajnikamrajnikam@gmail.com>
2024-06-08 23:18:40 +0530

    Add completion and snippets
    - Credits to the typecraft series, might look into the debugger too
    - lsp snippets, vscode-community driven snippets, and general snippets

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -17,7 +17,10 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
         end
