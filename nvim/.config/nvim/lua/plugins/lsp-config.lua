6fcd7d74a44baef62ffbd7e6f1d642b5018dda69
craniacshencil <rajnikamrajnikam@gmail.com>
2024-06-17 21:14:42 +0530

    Change dashboard art, credits Typeraft

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -3,32 +3,32 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pylsp", "gopls"}
+                ensure_installed = { "lua_ls", "pylsp", "gopls" },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             lspconfig.lua_ls.setup({
                 capabilities = capabilities
             })
             lspconfig.pylsp.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             lspconfig.gopls.setup({
-                capabilities = capabilities
+                capabilities = capabilities,
             })
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
-        end
-    }
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
