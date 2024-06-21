98b06960057348f719ecd4c3f7771db32c98f62f
craniacshencil <rajnikamrajnikam@gmail.com>
2024-06-21 23:33:19 +0530

    Add autocommand for auto-format on save

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
@@ -12,7 +13,22 @@ return {
 				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.prettierd,
 			},
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({
+                        group = augroup,
+                        buffer = bufnr,
+                    })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({bufnr = bufnr})
+                        end
+                    })
+                end
+            end
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		-- vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 	end,
 }
