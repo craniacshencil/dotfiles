f519d79a8f596e414a960eb35bec05e147579e16
craniacshencil <rajnikamrajnikam@gmail.com>
2024-11-30 09:22:32 +0530

    Add gruvbox-material plugin in nvim

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -1,12 +1,15 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  name = "gruvbox",
+  "sainnhe/gruvbox-material",
+  lazy = false,
   priority = 1000,
   config = function()
-    local gruv = require("gruvbox")
-    gruv.setup({
-      transparent_mode = true,
-    })
-    vim.cmd.colorscheme("gruvbox")
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_transparent_background = 2
+    vim.g.gruvbox_material_foreground = "material"
+    vim.g.gruvbox_material_enable_bold = 1
+    vim.g.gruvbox_material_menu_inlay_hints_backgroudnd = "dimmed"
+    vim.g.vim.cmd.colorscheme("gruvbox-material")
   end,
 }
