9aa0038df5d68e321b1335a5a13b792f58914c43
craniacshencil <rajnikamrajnikam@gmail.com>
2024-11-30 09:38:20 +0530

    Add mdx syntax highlighting plugin

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -1,15 +1,13 @@
 return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_transparent_background = 2
-    vim.g.gruvbox_material_foreground = "material"
-    vim.g.gruvbox_material_enable_bold = 1
-    vim.g.gruvbox_material_menu_inlay_hints_backgroudnd = "dimmed"
-    vim.g.vim.cmd.colorscheme("gruvbox-material")
-  end,
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_enable_italic = true
+		vim.g.gruvbox_material_transparent_background = 2
+		vim.g.gruvbox_material_foreground = "mix"
+		vim.g.gruvbox_material_enable_bold = 1
+		vim.g.gruvbox_material_menu_inlay_hints_backgroudnd = "dimmed"
+		vim.cmd.colorscheme("gruvbox-material")
+	end,
 }
