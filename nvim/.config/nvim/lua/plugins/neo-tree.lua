daa16adb220cbf12c33e7f3e1d6a45a1b85aeeba
craniacshencil <rajnikamrajnikam@gmail.com>
2025-12-30 23:35:44 +0530

    Change neotree keymap
    <leader>t was slow, waiting for completion i.e why

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,8 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.keymap.set("n", "<Leader>t", ":Neotree filesystem reveal right<CR>", { noremap = true })
+		-- noh<CR> only because there is a 'n' in the map
+		vim.keymap.set("n", "<Leader>tn", ":Neotree filesystem reveal right<CR> :noh<CR>", { noremap = true })
 		vim.keymap.set("n", "<Leader>tq", ":Neotree filesystem close<CR>", { noremap = true })
 	end,
 }
