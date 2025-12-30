a1f01213985c46e410f9e6842a296fbcea37529f
craniacshencil <rajnikamrajnikam@gmail.com>
2025-12-30 22:13:00 +0530

    Add gitsigns for gutter signs
    - Change harpoon keymap

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,29 +1,29 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-  config = function()
-    local harpoon = require("harpoon")
-    harpoon:setup()
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-    vim.keymap.set("n", "<leader>+", function()
-      harpoon:list():add()
-    end)
-    vim.keymap.set("n", "<leader>h", function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+		vim.keymap.set("n", "<leader>+", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>hn", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
 
-    vim.keymap.set("n", "<leader>1", function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set("n", "<leader>2", function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set("n", "<leader>3", function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set("n", "<leader>4", function()
-      harpoon:list():select(4)
-    end)
-  end,
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end)
+	end,
 }
