04b08c9ab64ab1c20a63972b2cf6641621717e67
craniacshencil <rajnikamrajnikam@gmail.com>
2026-04-22 14:48:54 +0530

    Remove autoformat on save
    - Add binding for readest

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -2,7 +2,7 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
@@ -16,17 +16,12 @@ return {
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({
-						group = augroup,
-						buffer = bufnr,
-					})
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
+					vim.keymap.set("n", "<leader>f", function()
+						vim.lsp.buf.format({
+							bufnr = bufnr,
+							async = false,
+						})
+					end, { buffer = bufnr, desc = "Format buffer" })
 				end
 			end,
 		})
